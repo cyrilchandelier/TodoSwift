7faa5a46b4e0088f79e3bc803487d9d3ac4ac6fb
Cyril Chandelier <cyril.chandelier@gmail.com>
2015-10-31 10:58:44 +0100

    Add a new empty project

---
+++ Code/TodoSwiftKit/TodoSwiftKit.h
@@ -0,0 +1,19 @@
+//
+//  TodoSwiftKit.h
+//  TodoSwiftKit
+//
+//  Created by Cyril Chandelier on 31/10/15.
+//  Copyright © 2015 Cyril Chandelier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TodoSwiftKit.
+FOUNDATION_EXPORT double TodoSwiftKitVersionNumber;
+
+//! Project version string for TodoSwiftKit.
+FOUNDATION_EXPORT const unsigned char TodoSwiftKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TodoSwiftKit/PublicHeader.h>
+
+
